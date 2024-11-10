9d34a252ebfc906a37a65b38f2556a400cceea1b
Jose Garcia <jmagarciadiez@gmail.com>
2024-11-10 18:44:35 -0300

    Apply formatter

--- src/models/Mascota.ts
+++ src/models/Mascota.ts
@@ -1,27 +1,27 @@
 import { GeneradorID } from "./GeneradorID";
 import { Cliente } from "./Cliente";
 
-export class Mascota{
-    private ID: string;
-    private nombre: string;
-    private especie: string[];
-    private nuevaEspecie: string[];
-    public constructor(nombre: string){
-        this.nombre = nombre;
-        this.especie = ["Gato","Perro","Exótica"];
-        this.nuevaEspecie = [];
-        this.ID = "";
-    }
-    public getEspecie(){
-        return this.especie;
-    }
-    public getID(){
-        return this.ID;
-    }
-    public setEspecie(i: number){
-        this.nuevaEspecie.push(this.especie[i]);
-    }
-    public asignarID(ClienteID: string){
-       return this.ID = ClienteID; 
-    }
+export class Mascota {
+  private ID: string;
+  private nombre: string;
+  private especie: string[];
+  private nuevaEspecie: string[];
+  public constructor(nombre: string) {
+    this.nombre = nombre;
+    this.especie = ["Gato", "Perro", "Exótica"];
+    this.nuevaEspecie = [];
+    this.ID = "";
+  }
+  public getEspecie() {
+    return this.especie;
+  }
+  public getID() {
+    return this.ID;
+  }
+  public setEspecie(i: number) {
+    this.nuevaEspecie.push(this.especie[i]);
+  }
+  public asignarID(ClienteID: string) {
+    return (this.ID = ClienteID);
+  }
 }
