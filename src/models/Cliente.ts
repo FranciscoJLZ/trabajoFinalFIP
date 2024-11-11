f67e6e2a0eaeee7c51071464ddfa95d4e03c35fa
Jose Garcia <jmagarciadiez@gmail.com>
2024-11-11 09:03:57 -0300

    Add code review

--- src/models/Cliente.ts
+++ src/models/Cliente.ts
@@ -27,10 +27,23 @@ export class Cliente {
     }
   }
 
+  // Aca estamos esperando por parametro un string para luego agregarlo a un array que esta
+  // declarado para guardar objetos de tipo Mascota, entonces por parametro tendrias
+  // que esperar un objeto de tipo Mascota.
+  // Tambien, al solo efecto de hacer mas robusto el codigo, agregaria un chequeo de que
+  // el objeto a agregar ya no este agregado.
   public agregarMascota(mascota: string): void {
     this.mascotas.push(mascota);
   }
 
+  // Aca pasa lo mismo, estas tratando a mascota como si fuera de tipo string, y es
+  // de tipo Mascota.
+  // Despues de eso, deberias acceder a un metodo getter de mascota para comparar
+  // con el parametro que vas a utilizar para filtrar, por ejemplo:
+  // this.mascotas.filter((mascota) => mascota.getNombre() !=== nombre)
+  // Tambien comprar por nombre puede ser problematico, en caso de que un cliente
+  // tenga dos mascotas que se llamen iguales. Le sugeri a FZ que agregue un id unico
+  // a la clase Mascota para este tipo de casos.
   public bajaMascota(nombre: string): void {
     this.mascotas = this.mascotas.filter((mascota) => mascota != nombre);
   }
