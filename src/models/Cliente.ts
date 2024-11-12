e161a7b558943a94d915e32d1922570bdf17f711
FranciscoFernandez90 <fernandez.francisco.a@gmail.com>
2024-11-12 10:35:41 -0300

    correcciones a la clase cliente

--- src/models/Cliente.ts
+++ src/models/Cliente.ts
@@ -1,45 +1,65 @@
-import {GeneradorId} from "./GeneradorID"
-import {Mascota} from "./Mascota"
+import { GeneradorID } from "./GeneradorID";
+import { Mascota } from "./Mascota";
 
 export class Cliente {
-    protected id:string;
-    protected nombre:string;
-    protected telefono:number;
-    protected visitas:number;
-    protected esVip:string;
-    protected mascotas:Mascota[]
+  protected id: string;
+  protected nombre: string;
+  protected telefono: number;
+  protected visitas: number;
+  protected esVip: string;
+  protected mascotas: Mascota[];
 
-    constructor(nombre:string,telefono:number) {
-        this.id= GeneradorID.instancia.generarID();
-        this.nombre=nombre;
-        this.telefono=telefono;
-        this.visitas=0;
-        this.esVip="No";
-        this.mascotas=[];
+  public constructor(nombre: string, telefono: number) {
+    this.id = GeneradorID.instancia.generarID();
+    this.nombre = nombre;
+    this.telefono = telefono;
+    this.visitas = 0;
+    this.esVip = "No";
+    this.mascotas = [];
+  }
+  public getID() {
+    return this.id;
+  }
+  public agregarVisita(): void {
+    this.visitas = this.visitas + 1;
+    if (this.visitas >= 5) {
+      this.esVip = "Si";
     }
+  }
 
-    agregarVisita():void{
-     this.visitas=this.visitas+1
-     if (this.visitas>=5){
-        this.esVip="Si"
-     }
+  
+  public agregarMascota(mascota: Mascota): void {
+    this.mascotas.push(mascota);
+    mascota.asignarID(this.id)
+  }
+
+  
+  public bajaMascota(nombre: Mascota): void {
+    this.mascotas = this.mascotas.filter(mascota => mascota != nombre);
+  }
+
+  public modificarCliente(propiedadAModificar: string, valorNuevo: any) {
+    if (propiedadAModificar == "nombre") {
+      this.nombre = valorNuevo;
+    } else {
+      this.telefono = valorNuevo;
     }
-
-    agregarMascota(mascota:string):void{
-        this.mascotas.push(mascota)
-    }
-
-    bajaMascota(nombre:string):void{
-        this.mascotas=this.mascotas.filter(mascota => mascota !=nombre)
-    }
-
-    modificarCliente(propiedadAModificar:string,valorNuevo:any){
-        if (propiedadAModificar=="nombre"){
-            this.nombre=valorNuevo
-        }else {
-            this.telefono=valorNuevo
-        }
-    }
-
-
+  }
 }
+
+
+let cliente1:Cliente= new Cliente("Juan", 2284556633)
+let mascota1:Mascota= new Mascota("firu")
+let mascota2:Mascota= new Mascota ("pelos")
+cliente1.agregarMascota(mascota1)
+cliente1.agregarMascota(mascota2)
+
+console.log(cliente1)
+
+cliente1.bajaMascota(mascota1)
+cliente1.agregarVisita()
+cliente1.agregarVisita()
+cliente1.agregarVisita()
+cliente1.agregarVisita()
+cliente1.agregarVisita()
+console.log(cliente1)
